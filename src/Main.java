c2371fcb0dc672aa47e6f60bdf4aa8531b6cc6d3
Denys Kovtun <paryya28@gmail.com>
2023-08-09 20:43:35 +0200

    feat: add runStarMenu()

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    runStartMenu();
+  }
+  public static void runStartMenu(){
+    System.out.println("Добро пожаловать в 'Список дел' ");
+    System.out.println("Выберите действие...");
+
+    List <String> allOptions = new ArrayList<>();
+    allOptions.add(" добавить в список");
+    allOptions.add(" удалить из списка");
+    allOptions.add(" изменить значение");
+    allOptions.add(" выход");
+    int num = 0;
+    int choice;
+    for (String option:allOptions){
+      num++;
+      System.out.println(num+option);
+    }
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(scanner);
+    choice = scanner.nextInt();
+
+    System.out.println(choice);
   }
 }
