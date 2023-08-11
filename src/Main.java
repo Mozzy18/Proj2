6c09d0b4510934e5e23dd0d0a33826d8de15f816
Valeriy Ostrowerhow <valeriy22@web.de>
2023-08-11 18:55:03 +0200

    add Neue Menu

--- src/Main.java
+++ src/Main.java
@@ -16,20 +16,31 @@ public class Main {
   }
 
   public static void runStartMenu() {
-    System.out.println("Добро пожаловать в 'Список дел' ");
-    System.out.println("Выберите действие...");
+    System.out.println();
+    System.out.println(" ═-═-═-═-═-═ Добро пожаловать в Список дел ═-═-═-═-═-═ ");
 
+    System.out.println(" =====================  〚 МЕНЮ 〛 ====================");
+    System.out.println();
     List<String> allOptions = new ArrayList<>();
-    allOptions.add(" добавить в список");
-    allOptions.add(" удалить из списка");
-    allOptions.add(" изменить значение");
-    allOptions.add(" выход");
+
+    allOptions.add(". Добавить новое задание в список");
+
+    allOptions.add(". Удалить задание из списка");
+
+    allOptions.add(". Изменить задание или дату выполнения");
+
+    allOptions.add(". Выход");
+
     int num = 0;
     int choice;
     for (String option : allOptions) {
       num++;
       System.out.println(num + option);
     }
+    System.out.println();
+    System.out.println("Введите номер пункта меню: ");
+    System.out.println();
+
     Scanner scanner = new Scanner(System.in);
     System.out.println(scanner);
     choice = scanner.nextInt();
